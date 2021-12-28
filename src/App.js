75ae77e668d2eeec549123dfae30e8c1884bb7f3
Nahuel Paladino <43184105+npala043@users.noreply.github.com>
2021-12-28 16:12:39 -0700

    App imports tags and genres + sorts them

--- src/App.js
+++ src/App.js
@@ -4,17 +4,15 @@ import Header from './components/Header';
 import Form from './components/Form';
 import axios from 'axios';
 
+import tags from './assets/json/tags.json';
+import genres from './assets/json/genres.json';
 
 function App() {
 
-  const [tags, setTags] = useState([]);
-
-  const tagsUrl = 'https://api.rawg.io/api/tags?page_size=40&key=9475b06bb69f43b7a4e3a481475a6ce8';
-  axios.get(tagsUrl)
-    .then(response => {
-
-    })
-    .catch(error => console.log(error));
+  tags.sort((a, b) => a.id - b.id);
+  genres.sort((a, b) => {
+    return a.name.toUpperCase() < b.name.toUpperCase() ? -1 : 1;
+  });
 
   return (
     <div className="App">
