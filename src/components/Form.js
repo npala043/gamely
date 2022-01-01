4b04f82466b7ddebec2e791be9a19239c3815a0e
Jun Young Won <JunYoung.Won@edu.sait.ca>
2022-01-01 02:46:28 -0700

    push form changes

--- src/components/Form.js
+++ src/components/Form.js
@@ -15,9 +15,9 @@ const Form = (props) => {
     let genres = [];
     let tags = [];
 
-    const handle = () => {
+    const handle = (ratings, platforms, genres, tags) => {
         // handle form submission
-        props.showResults();
+        props.showResults(ratings, platforms, genres, tags);
         console.log("Form submitted!!");
     }
 
@@ -30,49 +30,48 @@ const Form = (props) => {
             index = tagArray.indexOf(+e.target.value)
             tagArray.splice(index, 1)
         }
-        // console.log(tagArray[0])
+        console.log(tagArray)
     }
 
     return (
         <div>
-            {console.log(tags[0])}
             <Collapse>
                 <Panel header="What is your preferred rating?">
-                    <input type="checkbox" value="0" onChange={(e) => changeButton(e, ratings)}>E[Everyone]</input>
-                    <input type="checkbox" value="1" onChange={(e) => changeButton(e, ratings)}>Teen[Ages 13+]</input>
-                    <input type="checkbox" value="2" onChange={(e) => changeButton(e, ratings)}>Mature[Ages 18+]</input>
-                    <input type="checkbox" value="" onChange={(e) => changeButton(e, ratings)}>No preference</input>
+                    <input type="checkbox" value="0" onChange={(e) => changeButton(e, ratings)}/>E[Everyone]
+                    <input type="checkbox" value="1" onChange={(e) => changeButton(e, ratings)}/>Teen[Ages 13+]
+                    <input type="checkbox" value="2" onChange={(e) => changeButton(e, ratings)}/>Mature[Ages 18+]
+                    <input type="checkbox" value="" onChange={(e) => changeButton(e, ratings)}/>No preference
                 </Panel>
 
                 <Panel header="How many players?">
-                    <input type="checkbox" value="0" onChange={(e) => changeButton(e, tags)}>Single-Player</input>
-                    <input type="checkbox" value="1" onChange={(e) => changeButton(e, tags)}>Co-op(up to 4 players)</input>
-                    <input type="checkbox" value="2" onChange={(e) => changeButton(e, tags)}>Online Multiplayer</input>
-                    <input type="checkbox" value="" onChange={(e) => changeButton(e, tags)}>No preference</input>
+                    <input type="checkbox" value="0" onChange={(e) => changeButton(e, tags)}/>Single-Player
+                    <input type="checkbox" value="1" onChange={(e) => changeButton(e, tags)}/>Co-op(up to 4 players)
+                    <input type="checkbox" value="2" onChange={(e) => changeButton(e, tags)}/>Online Multiplayer
+                    <input type="checkbox" value="" onChange={(e) => changeButton(e, tags)}/>No preference
                 </Panel>
                
-                <Panel header="What price range?">
-                    <input type="checkbox" value="0" onChange={(e) => changeButton(e, tags)}>E[Everyone]</input>
-                    <input type="checkbox" value="1" onChange={(e) => changeButton(e, tags)}>Teen[Ages 13+]</input>
-                    <input type="checkbox" value="2" onChange={(e) => changeButton(e, tags)}>Mature[Ages 18+]</input>
-                    <input type="checkbox" value="" onChange={(e) => changeButton(e, tags)}>No preference</input>
+                <Panel header="How old do you prefer your games?">
+                    <input type="checkbox" value="0" onChange={(e) => changeButton(e, tags)}/>Classic (6 years and older)
+                    <input type="checkbox" value="1" onChange={(e) => changeButton(e, tags)}/>Recent (Less than 5 years old)
+                    <input type="checkbox" value="" onChange={(e) => changeButton(e, tags)}/>No preference
                 </Panel>
                 <Panel header="Select your favourite genre(s)">
-                    <input type="checkbox" value="0" onChange={(e) => changeButton(e, genres)}>E[Everyone]</input>
-                    <input type="checkbox" value="1" onChange={(e) => changeButton(e, genres)}>Teen[Ages 13+]</input>
-                    <input type="checkbox" value="2" onChange={(e) => changeButton(e, genres)}>Mature[Ages 18+]</input>
-                    <input type="checkbox" value="" onChange={(e) => changeButton(e, genres)}>No preference</input>
+                    <input type="checkbox" value="0" onChange={(e) => changeButton(e, genres)}/>Action
+                    <input type="checkbox" value="1" onChange={(e) => changeButton(e, genres)}/>Adventure
+                    <input type="checkbox" value="2" onChange={(e) => changeButton(e, genres)}/>Arcade
+                    <input type="checkbox" value="" onChange={(e) => changeButton(e, genres)}/>No preference
                 </Panel>
 
                 <Panel header="Which console(s)?">
-                    <input type="checkbox" value="0" onChange={(e) => changeButton(e, platforms)}>E[Everyone]</input>
-                    <input type="checkbox" value="1" onChange={(e) => changeButton(e, platforms)}>Teen[Ages 13+]</input>
-                    <input type="checkbox" value="2" onChange={(e) => changeButton(e, platforms)}>Mature[Ages 18+]</input>
-                    <input type="checkbox" value="" onChange={(e) => changeButton(e, platforms)}>No preference</input>
+                    <input type="checkbox" value="0" onChange={(e) => changeButton(e, platforms)}/>Mobile (android/ios)
+                    <input type="checkbox" value="1" onChange={(e) => changeButton(e, platforms)}/>Nintendo Switch
+                    <input type="checkbox" value="2" onChange={(e) => changeButton(e, platforms)}/>Xbox
+                    <input type="checkbox" value="3" onChange={(e) => changeButton(e, platforms)}/>Playstation
+                    <input type="checkbox" value="" onChange={(e) => changeButton(e, platforms)}/>Desktop PC
                 </Panel>
 
             </Collapse>
-            <button onClick={Games={ratings, platforms, genres, tags}}>Show my Games!</button>
+            <button onClick={(e) => handle(ratings, platforms, genres, tags)}>Show my Games!</button>
         </div>
     )
 }
